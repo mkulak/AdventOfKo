a607481bbf16d6aa47f736fec32b4501d2a07b94
mkulak <kleshney@gmail.com>
2021-12-11 21:51:12 +0100

    alternative solution for day 11 b

--- src/main/kotlin/day11.kt
+++ src/main/kotlin/day11.kt
@@ -1,42 +1,43 @@
 fun day11a(): Int {
-    val data = readInput("day11.txt").lines().map { it.map { it.code - '0'.code }.toIntArray() }
-    return (1..100).fold(0) { acc, _ -> acc + step(data) }
+    val data = readInput("day11.txt").lines().flatMap { it.map { it.code - '0'.code } }.toIntArray()
+    return (1..100).sumOf { step(data) }
 }
 
 fun day11b(): Int {
-    val data = readInput("day11.txt").lines().map { it.map { it.code - '0'.code }.toIntArray() }
-    return generateSequence { step(data) }.indexOfFirst { it == data.size * data[0].size } + 1
+    val data = readInput("day11.txt").lines().flatMap { it.map { it.code - '0'.code } }.toIntArray()
+    return generateSequence { step(data) }.indexOfFirst { it == data.size } + 1
 }
 
+val size = 10
 val flash = -1
 val max = 9
-val neighbours = listOf(-1 to -1, 0 to -1, 1 to -1, -1 to 0, 1 to 0, -1 to 1, 0 to 1, 1 to 1)
 
-fun step(data: List<IntArray>): Int {
-    for (y in data.indices) {
-        for (x in data[y].indices) {
-            inc(data, x, y)
-        }
-    }
+fun step(data: IntArray): Int {
+    data.indices.forEach { i -> inc(data, i) }
     var flashes = 0
-    for (y in data.indices) {
-        for (x in data[y].indices) {
-            if (data[y][x] == flash) {
-                flashes++
-                data[y][x] = 0
-            }
+    data.indices.forEach { i ->
+        if (data[i] == flash) {
+            flashes++
+            data[i] = 0
         }
     }
     return flashes
 }
 
-fun inc(data: List<IntArray>, x: Int, y: Int): Unit =
+fun inc(data: IntArray, i: Int): Unit =
     when {
-        y !in data.indices || x !in data[y].indices || data[y][x] == flash -> Unit
-        data[y][x] < max -> data[y][x] += 1
+        data[i] == flash -> Unit
+        data[i] < max -> data[i] += 1
         else -> {
-            data[y][x] = flash
-            neighbours.forEach { (dx, dy) -> inc(data, x + dx, y + dy) }
+            data[i] = flash
+            neighbours(i).forEach { inc(data, it) }
+        }
+    }
+
+fun neighbours(i: Int) =
+    (i / size - 1..i / size + 1).flatMap { ny ->
+        (i % size - 1..i % size + 1).mapNotNull { nx ->
+            (ny * size + nx).takeIf { ny in 0 until size && nx in 0 until size && it != i }
         }
     }
 
