5c6eabd60735e4508cd1c69d6c26a11b3e33d499
mkulak <kleshney@gmail.com>
2021-12-12 16:59:49 +0100

    beautify solution for day 7 b

--- src/main/kotlin/day7.kt
+++ src/main/kotlin/day7.kt
@@ -5,10 +5,7 @@ fun day7b(): Int {
     val data = readInput("day7.txt").trim().split(",").map { it.toInt() }
     val min = data.minOrNull()!!
     val max = data.maxOrNull()!!
-    return (min..max).fold(Int.MAX_VALUE) { acc, value ->
-        val cost = calcCost(data, value)
-        min(cost, acc)
-    }
+    return (min..max).minOf { calcCost(data, it) }
 }
 
 fun calcCost(data: List<Int>, value: Int): Int =
