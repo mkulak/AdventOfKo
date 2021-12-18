601cf74721dd84d1f44fe720c395d448bee66d2c
mkulak <kleshney@gmail.com>
2021-12-18 21:47:59 +0100

    "optimized" ugly solution for day 15

--- src/main/kotlin/day15.kt
+++ src/main/kotlin/day15.kt
@@ -1,44 +1,46 @@
 import java.util.PriorityQueue
-import kotlin.math.absoluteValue
 
 fun day15a(): Int {
-    val data = readInput("day15.txt").lines().map { line ->
-        line.map { it.code - '0'.code }.toIntArray()
-    }
-    val target = XY(data[0].size - 1, data.size - 1)
-    fun XY.neighbours() = listOf(XY(x - 1, y), XY(x + 1, y), XY(x, y - 1), XY(x, y + 1))
-    fun XY.isValid() = x in data[0].indices && y in data.indices
+    val lines = readInput("day15.txt").lines()
+    val data = lines.flatMap { line -> line.map { (it.code - '0'.code).toByte() } }.toByteArray()
+    val size = lines[0].length
+    val target = data.size - 1
 
-    data class State(val pos: XY, val cost: Int, val visited: Set<XY>)
-
-    var minPath = Int.MAX_VALUE
-    val comparator = compareBy<State> { (target.x - it.pos.x) + (target.y - it.pos.y) }
+    val minCostFound = IntArray(data.size) { Int.MAX_VALUE }
+    val comparator = compareBy<State> { (size - it.pos % size) + (size - it.pos / size) }
     val queue = PriorityQueue(comparator)
-    queue += State(XY(0, 0), -data[0][0], emptySet())
+    queue += State(0, -data[0], BooleanArray(data.size))
     while (queue.isNotEmpty()) {
         val state = queue.poll()
-        println(queue.size)
-        val newCost = state.cost + data[state.pos.y][state.pos.x]
-        if (newCost >= minPath) {
+        val newCost = state.cost + data[state.pos]
+        if (newCost >= minCostFound[state.pos]) {
             continue
         }
+        minCostFound[state.pos] = newCost
         if (state.pos == target) {
             println("new minPath: $newCost")
-            minPath = newCost
             continue
-//            return newCost
         }
-        val newVisited = state.visited + state.pos
-        state.pos.neighbours().forEach {
-            if (it.isValid() && it !in newVisited) {
-                queue += State(it, newCost, newVisited)
-            }
-        }
+        val newVisited = state.visited.copyOf()
+        newVisited[state.pos] = true
+        val x = state.pos % size
+        val y = state.pos / size
+        if (x > 0) queue.addState(state.pos - 1, newCost, state, newVisited)
+        if (x < size - 1) queue.addState(state.pos + 1, newCost, state, newVisited)
+        if (y > 0) queue.addState(state.pos - size, newCost, state, newVisited)
+        if (y < size - 1) queue.addState(state.pos + size, newCost, state, newVisited)
     }
-    return minPath
+    return minCostFound.last()
 }
 
-data class XY(val x: Int, val y: Int)
+data class State(val pos: Int, val cost: Int, val visited: BooleanArray)
+
+fun PriorityQueue<State>.addState(pos: Int, newCost: Int, state: State, newVisited: BooleanArray) {
+    if (!state.visited[pos]) {
+        newVisited[pos] = true
+        this += State(pos, newCost, newVisited)
+    }
+}
 
 fun main() {
     println(day15a())
