3c74287a9abb9f23a433fea764238606d728cca8
mkulak <kleshney@gmail.com>
2021-12-24 18:42:04 +0100

    beautify solution for day 22 a

--- src/main/kotlin/day21.kt
+++ src/main/kotlin/day21.kt
@@ -1,40 +1,28 @@
 
 fun day21a(): Int {
-    val (start1, start2) = readInput("day21.txt").lines().map { it.last().code - '0'.code }
-    var pos1 = start1
-    var pos2 = start2
-    var score1 = 0
-    var score2 = 0
-    var throwCount = 0
-    var dice = 0
-    fun throwDice(): Int {
-        throwCount++
-        dice = dice % 100 + 1
-        println("throw $throwCount dice: $dice")
-        return dice
-    }
-    fun makeTurn(pos: Int): Int {
-        val inc = (throwDice() + throwDice() + throwDice()) % 10
-        val newPos = pos + inc
-        return if (newPos > 10) newPos - 10 else newPos
+    val (pos1, pos2) = readInput("day21.txt").lines().map { it.last().code - '0'.code }
+    val pos = intArrayOf(pos1, pos2)
+    val scores = intArrayOf(0, 0)
+    var turn = 0
+    fun makeTurn() {
+        val player = turn % 2
+        val inc = (((turn * 3) % 100) * 3 + 6) % 10
+        val newPos = pos[player] + inc
+        pos[player] = if (newPos > 10) newPos - 10 else newPos
+        scores[player] += pos[player]
+        turn++
     }
     while (true) {
-        pos1 = makeTurn(pos1)
-        score1 += pos1
-        println("Player 1 moves to $pos1 for a total score of $score1")
-        if (score1 >= 1000) {
-            return score2 * throwCount
-        }
-        pos2 = makeTurn(pos2)
-        score2 += pos2
-        println("Player 2 moves to $pos2 for a total score of $score2")
-        if (score2 >= 1000) {
-            return score1 * throwCount
+        val player = turn % 2
+        makeTurn()
+//        println("Player ${player + 1} moves to ${pos[player]} for a total score of ${scores[player]}")
+        if (scores[player] >= 1000) {
+            return scores[(player + 1) % 2] * turn * 3
         }
     }
 }
-
 
 fun main() {
     println(day21a())
 }
+
